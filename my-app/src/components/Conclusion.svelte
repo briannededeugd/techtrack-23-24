164044c3035acac880846aa2b7aa311caa3c61ac
Brianne <92082302+briannededeugd@users.noreply.github.com>
2023-11-29 19:08:38 +0100

    FEAT: finish conclusion

--- my-app/src/components/Conclusion.svelte
+++ my-app/src/components/Conclusion.svelte
@@ -11,7 +11,16 @@
 	<section>
 		<p>
 			time, they've always been present at the writer's table and behind the
-			camera.
+			camera. In 100 years, the percentage women make up in the crews of movies
+			made has grown from a mere 4.68% to 12.13%, and since 1985 women have been
+			getting nominated for Best Director and Best Cinematography at the Oscars!
+			And although the numbers may not be very large yet, the journey we have
+			just now taken through time proves that there's a steady increase in
+			female representation in film.<br />
+
+			<br />I hope these visualisations motivate you to pay more attention to
+			who is making your movies, and what that means for how that movie turned
+			out in the end. After all, women can do anything.
 		</p>
 	</section>
 </section>
