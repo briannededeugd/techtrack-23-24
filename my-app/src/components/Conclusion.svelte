2380984c4a5c2010c16e463b0e97c94bca6b8874
Brianne <92082302+briannededeugd@users.noreply.github.com>
2023-11-29 12:32:27 +0100

    FEAT: conclusion layout

--- my-app/src/components/Conclusion.svelte
+++ my-app/src/components/Conclusion.svelte
@@ -1,15 +1,16 @@
 <section class="entirepage">
 	<section>
 		<h2>
-			<span>W</span>hat Can We L<span>ea</span>rn Fr<span>o</span>m This Data?
+			In C<span>o</span>nclusi<span>o</span>n:
 		</h2>
+		<p>
+			As we've seen in the data, women haven't been entirely invisible in the
+			twentieth century film world. Though they weren't winning Oscars at the
+		</p>
 	</section>
 	<section>
 		<p>
-			As we've seen in the data, the contribution of women has significantly
-			increased in the last 100 years, but women haven't been entirely invisible
-			in the twentieth century film world. Though they weren't winning Oscars at
-			the time, they've always been present at the writer's table and behind the
+			time, they've always been present at the writer's table and behind the
 			camera.
 		</p>
 	</section>
@@ -23,19 +24,10 @@
 		line-height: 0.8em;
 	}
 
-	.entirepage {
+	.entirepage section:first-of-type {
 		display: flex;
 		flex-direction: row;
 		gap: 4em;
 		max-width: 100%;
-		/* padding: 0 20vw; */
-	}
-	.entirepage > section:first-of-type {
-		max-width: 60%;
-		padding-right: 4em;
-	}
-
-	.entirepage > section:nth-of-type(2) {
-		max-width: 35%;
 	}
 </style>
